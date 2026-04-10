e1194f1529fe4fb92c5a8dc208c1a7622c2f7020
Amina Ossama <aminaossama@Aminas-MacBook-Air.local>
2026-04-10 21:40:33 +0200

    data file

---
+++ struct.h
@@ -0,0 +1,8 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+#include <string>
+using namespace std;
+struct vehicle{
+    int id;
+} ;
+#endif
--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    cout<<"hi";
-}
+#include <iostream>
+#include "struct.h"
+vehicle v;
+cin>>v.id;
+cout<<v.id;
